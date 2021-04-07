4cdbfe676f6cfd3c6887a949a511b64efe6abc9b
Malik <djohndev44@gmail.com>
2021-04-06 18:40:24 -0700

    Clean Code

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true
+    },
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 12,
+        'sourceType': 'module'
+    },
+    'rules': {
+        'quotes':['warn', 'single'],
+        'no-extra-semi': 'warn'
+    }
+};
